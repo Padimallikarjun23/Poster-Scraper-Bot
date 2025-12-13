200f122aa6156a4bf25c321b7e27d221e0fab539
the-piro <thezake0077@gmail.com>
2025-12-13 08:14:10 +0530

    Update __main__.py

--- echobotz/__main__.py
+++ echobotz/__main__.py
@@ -1,5 +1,7 @@
-import os
+# ruff: noqa: E402
+
 import asyncio
+import os
 from datetime import datetime
 from logging import Formatter
 
@@ -12,6 +14,7 @@ from .core.EchoClient import EchoBot
 from .core.plugs import add_plugs
 from .helper.utils.db import database
 from .helper.utils.bot_cmds import _get_bot_commands
+
 try:
     from web import _start_web, _ping
     WEB_OK = True
@@ -19,62 +22,37 @@ except ImportError:
     WEB_OK = False
 
 
-def main():
+bot_loop = asyncio.get_event_loop()
+
+
+async def main():
     def changetz(*args):
         return datetime.now(tz(Config.TIMEZONE)).timetuple()
 
     Formatter.converter = changetz
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(database._load_all())
+    await database._load_all()
 
     EchoBot.start()
     EchoBot.set_bot_commands(_get_bot_commands())
     LOGGER.info("Bot Cmds Set Successfully")
+
     me = EchoBot.get_me()
     LOGGER.info(f"Echo Bot Started as: @{me.username}")
 
-    if os.path.isfile(".restartmsg"):
-        try:
-            with open(".restartmsg") as f:
-                chat_id, msg_id = map(int, f.read().splitlines())
-
-            now = datetime.now(tz(Config.TIMEZONE)).strftime(
-                "%d/%m/%Y %I:%M:%S %p"
-            )
-
-            EchoBot.edit_message_text(
-                chat_id=chat_id,
-                message_id=msg_id,
-                text=f"<b>Restarted Successfully!</b>\n<code>{now}</code>",
-                disable_web_page_preview=True,
-            )
-
-            os.remove(".restartmsg")
-        except Exception as e:
-            LOGGER.error(f"Restart notify error: {e}")
-
     add_plugs()
-    
+
     if Config.WEB_SERVER and WEB_OK:
         LOGGER.info("Starting web server...")
-        asyncio.create_task(_start_web())
-        asyncio.create_task(_ping(Config.PING_URL, Config.PING_TIME))
+        bot_loop.create_task(_start_web())
+        bot_loop.create_task(_ping(Config.PING_URL, Config.PING_TIME))
     else:
         LOGGER.info("Web server disabled")
-    
-    idle()
+
+    await idle()
 
     EchoBot.stop()
     LOGGER.info("Echo Client stopped.")
 
 
-if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        LOGGER.error(f"Error deploying: {e}")
-        try:
-            EchoBot.stop()
-        except Exception:
-            pass
+bot_loop.run_until_complete(main())
